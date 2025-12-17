746bec6a0a3db834a0bccde3594e77d9e4fca9d0
PRIYANSHU KUMAR <159457122+PRIYANSHU4061@users.noreply.github.com>
2025-12-17 15:06:37 +0530

    Second Version

--- Remort.java
+++ Remort.java
@@ -1,1 +1,8 @@
-This is new file and im adding it from remort server
+//This is new file and im adding it from remort server
+class Remort{
+    public static void main(String[] args) {
+        int a  = 5;
+        int b = 6;
+        System.out.println(a+b);
+    }
+}

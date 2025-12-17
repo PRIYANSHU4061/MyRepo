57778956ac5f3d36195435be6401391eff904764
PRIYANSHU KUMAR <159457122+PRIYANSHU4061@users.noreply.github.com>
2025-12-17 15:34:53 +0530

    Added design in my website

--- Remort.java
+++
@@ -1,8 +0,0 @@
-//This is new file and im adding it from remort server
-class Remort{
-    public static void main(String[] args) {
-        int a  = 5;
-        int b = 6;
-        System.out.println(a+b);
-    }
-}
